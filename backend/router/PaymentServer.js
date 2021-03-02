f332845f3c929dd9a1e1c8ee93c73ba1338160fd
DanielQA123 <DAhuchogu@QA.com>
2021-03-02 14:10:03 +0000

    DA: Edited the server backendfor payment

--- backend/router/PaymentServer.js
+++ backend/router/PaymentServer.js
@@ -5,13 +5,13 @@ app.use(express.static('.'));
 
 const cinemaDomain = 'http://localhost:5019';
 
-app.post('/create-payment-checkout-session', async (req, res) => {
+app.post('/create-checkout-session', async (req, res) => {
     const session = await stripe.checkout.sessions.create({
         payment_method_types: ['card'],
         line_items: [
             {
                 price_data: {
-                    currency:'gbp',
+                    currency: 'gbp',
                     booking_data: {
                         filmName: 'The Waterboy'
 
@@ -22,11 +22,33 @@ app.post('/create-payment-checkout-session', async (req, res) => {
             },
         ],
         mode: 'payment',
-        success_url:`${cinemaDomain}?success=true`,
-        cancel_url:`${cinemaDomain}?canceled=true`,
+        success_url: `${cinemaDomain}?success=true`,
+        cancel_url: `${cinemaDomain}?canceled=true`,
     });
 
     res.json({ id: session.id });
 });
+
+app.use(express.json());
+const calculateOrderAmount = items => {
+    // Replace this constant with a calculation of the order's amount
+    // Calculate the order total on the server to prevent
+    // people from directly manipulating the amount on the client
+    return 1400;
+};
+
+app.post("/create-payment-intent", async (req, res) => {
+    const { items } = req.body;
+    // Create a PaymentIntent with the order amount and currency
+    const paymentIntent = await stripe.paymentIntents.create({
+        amount: calculateOrderAmount(items),
+        currency: "gbp"
+    });
+    res.send({
+        clientSecret: paymentIntent.client_secret
+    });
+});
+
+
 
 app.listen(5019, () => console.log('Running on port 5019'));
