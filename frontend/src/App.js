5b2fbeabea408c2b849913818567dec784154669
Waleed <WTahir@qa.com>
2021-03-03 15:59:30 +0000

    Screens page completed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,9 @@
 import './resources/App.css';
-
+import Screenparent from './Components/Screens/screenparent';
 function App() {
   return (
     <div className="App">
+      <Screenparent/>
       
     </div>
   );
---
+++ frontend/src/components/Screens/screenparent.jsx
@@ -0,0 +1,17 @@
+import Screens from "./screens"
+
+const Screenparent = () => {
+    return (
+        <>
+        <section>
+            <div className="container text-center">
+            <h2>Screens</h2>
+            </div>
+        </section>
+        <br></br>
+        <Screens/>
+        </>
+    );
+}
+
+export default Screenparent
