71518d63902abd7b38ae8c0608aad2db1c8b9d78
DanielQA123 <DAhuchogu@QA.com>
2021-03-04 14:37:26 +0000

    DA: Added title waiting for navbar

--- frontend/src/components/Getting-There/GettingThere.jsx
+++ frontend/src/components/Getting-There/GettingThere.jsx
@@ -4,7 +4,9 @@ const GettingThere =()=>{
     return(
         <>
         <section>
+            <div className="container text-center">
             <h2>Cinema Location </h2>
+            </div>
         </section>
 
 
