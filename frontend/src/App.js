342c23f1fa2ad9b723259688bc6e02555c5b9bcf
DanielQA123 <DAhuchogu@QA.com>
2021-03-04 14:37:05 +0000

    Changed import for App.jsx

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import Classifications from './components/Classifications/Classifications';
 import ListingsGallery from './components/Galleries/ListingsGallery';
 import NewReleasesGallery from './components/Galleries/NewReleasesGallery';
+import GettingThere from './components/Getting-There/GettingThere';
 import OpeningTime from './components/Opening-Times/CinemaTime';
 import PlacesToGo from './components/PlacesToGo/PlacesToGo';
 import './resources/App.css';
@@ -12,7 +13,8 @@ function App() {
       {/* <ListingsGallery /> */}
       {/* <NewReleasesGallery /> */}
       {/* <OpeningTime/> */}
-      <PlacesToGo/>
+      {/* <PlacesToGo/> */}
+      <GettingThere/>
     </div>
   );
 }
