cc0430385a4b60df5ddb436cc4d2a5098fec1e5d
DanielQA123 <DAhuchogu@QA.com>
2021-03-08 17:08:39 +0000

    DA: Adding jsx files for search bar

--- frontend/src/components/SearchBar/Search.jsx
+++ frontend/src/components/SearchBar/Search.jsx
@@ -1,7 +1,17 @@
+import {Button} from 'react-bootstrap';
 
-const Search = () => {
+const Search = (props) => {
+
+    const { stateQuery, updateQuery } = props;
+
     return (
         <>
+        <div>
+        <input type="text" placeholder="Type here to search" onChange={updateQuery} value={stateQuery} />
+            <Button variant="info" type="submit">Search</Button>{' '}
+        </div>
+            
+
             <div>Hello world!</div>
             <p>Hey Dan-Dan</p>
         </>
