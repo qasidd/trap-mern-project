29f878c15148d617fc66b5ba1584ebe38309b399
Hamza P <hpervez@qa.com>
2021-03-08 15:03:17 +0000

    improved UI,added logo

--- frontend/src/components/Home/Home.jsx
+++ frontend/src/components/Home/Home.jsx
@@ -1,10 +1,12 @@
 'use strict';
+import logo from '../../resources/img/logo.jpg';
 
 const Home =()=>{
 return(
     <>
         <h1> Welcome to QA Cinemas! </h1>
         <br></br>
+        <img src={logo} alt="this is a logo"/>
         <h2> Redefining the cinema experience </h2>
         <br></br>
         <p> This is the homepage for QA cinemas. This page will provide information about movies, listings and upcoming releases. </p>
