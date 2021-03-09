25179c3feb2c5e484b85d702b83d2fbf1e0e93ff
Hamza P <hpervez@qa.com>
2021-03-09 16:12:46 +0000

    updated routes for all pages/added buttons

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,7 +10,6 @@ import Home from './components/Home/Home';
 import OpeningTime from './components/Opening-Times/CinemaTime';
 import './resources/App.css';
 import Nav from "./components/Routing/Nav";
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import About from './components/About/About';
 import React from "react";
 import { ImageBackground, StyleSheet, Text, View } from "react";
@@ -19,30 +18,53 @@ import PlacesToGo from './components/PlacesToGo/PlacesToGo';
 import NewBooking from './components/Booking/NewBooking';
 import PaymentSuccess from './components/Booking/PaymentSuccess';
 import PaymentCancelled from './components/Booking/PaymentCancelled';
-
+import Screenparent from './components/Screens/screenparent';
 
 function App() {
   return (
     <div className="App">
       <Router>
-      <Nav/>
-      <Switch>
-      <Route path="/" exact> 
-        <Home/>
-      </Route>
-      <Route path="/about">
-        <About/>
-      </Route>
-              <Route path="/listings-gallery">
-            <ListingsGallery />
-            {/* <NewReleasesGallery /> */}
+        <Nav />
+        <Switch>
+          <Route path="/" exact>
+            <Home />
+          </Route>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/listings-gallery">
+            <ListingsGallery/>
+          </Route>
+          <Route path="/new-releases">
+            <NewReleasesGallery/>
+          </Route>
+          <Route path="/opening-times">
+            <OpeningTime/>
+          </Route>
+          <Route path="/classification">
+            <Classifications/>
+          </Route>
+          <Route path="/screens">
+            <Screenparent/>
           </Route>
           <Route path="/new-booking" exact>
             <NewBooking />
           </Route>
+          <Route path="/location" exact>
+            <GettingThere/>
+          </Route>
+          <Route path="/placestogo" exact>
+            <PlacesToGo/>
+          </Route>
+          <Route path="/contactus" exact>
+            <ContactForm/>
+          </Route>
+          <Route path="/discussion" exact>
+            <Discussionboard/>
+          </Route>
           <Route path="/new-booking/success" component={PaymentSuccess} />
           <Route path="/new-booking/cancelled" component={PaymentCancelled} />
-      </Switch>
+        </Switch>
       </Router>
       {/*<Classifications /> */}
       {/* <ListingsGallery /> */}
